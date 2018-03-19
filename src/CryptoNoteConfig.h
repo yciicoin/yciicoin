5d614e023aa9adc51614d4389e97b0f99ca47670
yciicoin <37514150+yciicoin@users.noreply.github.com>
2018-03-19 12:38:00 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"217.23.12.78:17896",
-  //"185.2.82.95:17896",
+  "217.23.12.78:17896",
+  "185.2.82.95:17896",
 };
 
 struct CheckpointData {
